6377a0f602d2e166ba56aa3f07ada7a7fa91d479
Sunischal Nakka <23B91A6247@srkrec.ac.in>
2026-03-08 23:19:04 +0530

    switch to filess.io database, clean up config

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -10,9 +10,6 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false  
-  }
 });
 
 module.exports = pool;
