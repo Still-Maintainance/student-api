e5971edf436e2565b273436098d46bc7ff430add
Sagar <sagaraddagarla@gmail.com>
2026-03-08 15:43:09 +0530

    deploy chnages

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,10 +1,12 @@
-const mysql = require('mysql2/promise');
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'student_db',
+  host: "hopper.proxy.rlwy.net",
+  port: 26243,
+  user: "root",
+  password: "SgMBhxbTeuJxLxJjxLNglOpvNUDkLisg",
+  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
