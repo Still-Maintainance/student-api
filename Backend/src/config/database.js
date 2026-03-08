e9a9e48cdc168797121ca9642163ff14c5cba0ff
Sagar <sagaraddagarla@gmail.com>
2026-03-08 15:46:20 +0530

    changes

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "hopper.proxy.rlwy.net",
-  port: 26243,
-  user: "root",
-  password: "SgMBhxbTeuJxLxJjxLNglOpvNUDkLisg",
-  database: "railway",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
